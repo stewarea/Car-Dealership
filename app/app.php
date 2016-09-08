612e7f040b9a768a4881ad3a083211bf0d3fc575
evan stewart <stewarea@gmail.com>
2016-09-08 15:50:40 -0700

    omg

--- app/app.php
+++ app/app.php
@@ -1,8 +1,7 @@
-<?php
+</<?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Car.php";
     $app = new Silex\Application();
-
     $app->get("/", function() {
         return
         "<!DOCTYPE html>
@@ -14,85 +13,55 @@
         <body>
             <div class='container'>
                 <h1>Find a Car!</h1>
-                <form action='Car.php'>
+                <form action='/car'>
                     <div class='form-group'>
-                        <label for='car_model'>Enter Model:</label>
-                        <input id='car_model' name='car_model' class='form-control' type='text'>
-                        <label for='car_price'>Enter Maximum Price:</label>
-                        <input id='car_price' name='car_price' class='form-control' type='number'>
-                        <label for='car_miles'>Enter Maximum Miles:</label>
-                        <input id='car_miles' name='car_miles' class='form-control' type='number'>
+                        <label for='price'>Enter Maximum Price:</label>
+                        <input id='price' name='price' class='form-control' type='number'>
                     </div>
+                    <div class='form-group'>
+                      <label for='mileage'>Enter Maximum Mileage:</label>
+                      <input id='mileage' name='mileage' class='form-control' type='number'>
                     <button type='submit' class='btn-success'>Submit</button>
                 </form>
             </div>
         </body>
-        </html>"
-        ;
+        </html>";
     });
-
     $app->get("car", function() {
-      $porsche = new Car("2014 Porsche 911", 114991, 7864);
-      $ford = new Car("2011 Ford F450", 55995, 14241);
-      $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-      $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
-
-      $cars = array($porsche, $ford, $lexus, $mercedes);
-
-      $cars_matching_search = array();
-      // foreach ($cars as $car) {
-      //     if ($car->price < $_GET['car_price'])  {
-      //         array_push($cars_matching_search, $car);
-      //       }
-      //     }
-      // foreach ($cars as $car) {
-      //     if ($car->make_model == $_GET['car_model']) {
-      //         array_push($cars_matching_search, $car);
-      //     }
-      //   }
-      // foreach ($cars as $car) {
-      //     if ($car->miles < $_GET['car_miles']) {
-      //         array_push($cars_matching_search, $car);
-      //     }
-      //   }
-      function worthBuying($max_price, $max_miles)
-      {
-          if ($this->price < ($max_price + 100) && $this->miles < $max_miles) {
-              return true;
-          } else {
-              return false;
-          }
-      }
-
-      $randomName = "<!DOCTYPE html>
-      <html>
-      <head>
-          <title>Your Car Dealership's Homepage</title>
-      </head>
-      <body>
-          <h1>Your Car Dealership</h1>
-          <ul>";
-      if (empty($cars_matching_search)) {
-          $randomName = $randomName."Enter a higher number.";
-      }
-
-      foreach ($cars_matching_search as $car) {
-
-          $randomName = $randomName."<li>".$car->getMAkeModel()."</li>".
-              "<ul>"
-                    "<li> $car->make_model </li>";
-                    "<li> $$car->price </li>";
-                    "<li> Miles: $car->miles </li>";
-              "</ul>";
+        $porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche911.jpg");
+        $ford = new Car("2011 Ford F450", 55995, 14241, "img/fordf450.jpg");
+        $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "img/lexus350.jpg");
+        $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "img/mercedescls550.jpg");
+        $cars = array($porsche, $ford, $lexus, $mercedes);
+        $cars_matching_search = array();
+        foreach ($cars as $car) {
+            if ($car->worthBuying($_GET['price'], $_GET['mileage'])) {
+                array_push($cars_matching_search, $car);
+            }
+        }
+        $randomName = "<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Your Car Dealership's Homepage</title>
+        </head>
+        <body>
+            <h1>Your Car Dealership</h1>
+            <ul>";
+        if (empty($cars_matching_search)) {
+            $randomName = $randomName."Enter a higher number you cheap bastard.";
+        }
+        foreach ($cars_matching_search as $car) {
+            $randomName = $randomName."<li>".$car->getMakeModel()."</li>".
+            "<ul>".
+                '<li> $'.$car->getPrice().'</li>'.
+                '<li> Miles: '.$car->getMiles().'</li>'.
+                "<li> <img src='".$car->getPicture()."'                     > </li>".
+            '</ul>';
         }
         $randomName = $randomName."</ul>
-
-      </body>
-      </html>"
-
-
-    return $randomName;
-  }):
-
+        </body>
+        </html>";
+        return $randomName;
+    });
     return $app;
-?>
+ ?>
--- src/Car.php
+++ src/Car.php
@@ -1,63 +1,56 @@
-
-
 <?php
-  class Car
-  {
-      private $make_model;
-      private $price;
-      private $miles;
-
-      function __construct($car_model, $car_price, $car_miles)
-      {
-          $this->make_model = $car_model;
-          $this->price = $car_price;
-          $this->miles = $car_miles;
-      }
-      function worthBuying($max_price, $max_miles)
-      {
-          if ($this->price < ($max_price + 100) && $this->miles < $max_miles) {
-              return true;
-          } else {
-              return false;
-          }
-      }
-      function getMakeModel()
-      {
-          return $this->make_model;
-      }
-      function setMakeModel($makeModel)
-      {
-          $this->make_model = $makeModel;
-      }
-      function getPrice()
-      {
-          return $this->price;
-      }
-      function setPrice($inputPrice)
-      {
-          $this->price = $inputPrice;
-      }
-      function getMiles()
-      {
-          return $this->miles;
-      }
-      function setMiles($inputMiles)
-      {
-          $this->miles = $inputMiles;
-      }
-      function getPicture()
-      {
-          return $this->picture;
-      }
-      function setPicture($inputPicture)
-      {
-          $this->picture = $inputPicture;
-      }
-  }
-
-
-
-
-
-
+class Car
+{
+    private $make_model;
+    private $price;
+    private $miles;
+    private $picture;
+    function __construct($make, $price, $miles, $image)
+    {
+        $this->make_model = $make;
+        $this->price = $price;
+        $this->miles = $miles;
+        $this->picture = $image;
+    }
+    function worthBuying($max_price, $max_miles)
+    {
+        if ($this->price < ($max_price + 100) && $this->miles < $max_miles) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function getMakeModel()
+    {
+        return $this->make_model;
+    }
+    function setMakeModel($makeModel)
+    {
+        $this->make_model = $makeModel;
+    }
+    function getPrice()
+    {
+        return $this->price;
+    }
+    function setPrice($inputPrice)
+    {
+        $this->price = $inputPrice;
+    }
+    function getMiles()
+    {
+        return $this->miles;
+    }
+    function setMiles($inputMiles)
+    {
+        $this->miles = $inputMiles;
+    }
+    function getPicture()
+    {
+        return $this->picture;
+    }
+    function setPicture($inputPicture)
+    {
+        $this->picture = $inputPicture;
+    }
+}
 ?>
